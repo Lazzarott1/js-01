845ddf31ead8ce732fa1fb43098d6b087d63f719
Lazzarott1 <lucas_lazzarotti@estudante.sesisenai.org.br>
2026-04-01 11:15:45 -0300

    add- 07

---
+++ ex07.js
@@ -0,0 +1,16 @@
+function tabela() {
+
+    let codigos = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+    let sabores = ["Chocolate", "Morango", "Creme", "Manga", "Melancia", "Vanilla Ice", "Céu Azul", "Brownie", "Hawaiano"];
+    let precos = ["1,50", "2,50", "2,50", "3,20", "3,40", "3,00", "3,60", "4,00", "5,00"];
+
+    let codigo = prompt("Digite o código do sabor (a-i):");
+
+    let indice = codigos.indexOf(codigo);
+
+    if (indice != -1) {
+        alert(sabores[indice] , " - R$ " , precos[indice]);
+    } else {
+        alert("Código inválido!");
+    }
+} 
